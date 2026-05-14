99d68c359360c107b31e634f0b5ef83470644e34
willi <willi@DESKTOP-HAGDQQ3>
2026-05-14 12:33:51 -0400

    Stubbed in test methods for AI Chat clients

--- src/test/java/com/wrgoff/ai_agent/controller/ChatControllerTest.java
+++ src/test/java/com/wrgoff/ai_agent/controller/ChatControllerTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.fail;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.Date;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,11 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.wrgoff.utils.DateUtils;
 
 @WebMvcTest(ChatController.class)
 class ChatControllerTest {
@@ -27,6 +34,9 @@ class ChatControllerTest {
     @Qualifier("googleAiClient")
     private ChatClient googleGenAiClient;
   
+    /**
+     * Test for Base Hello get method.
+     */
 	@Test
 	public void testHello() {
 		
@@ -48,4 +58,42 @@ class ChatControllerTest {
 			fail("Failed to get expected result for Testing of Hello <name>!");
 		}
 	}
+	
+	
+	/**
+	 * Test for Base get method that is used to call a caned request to Open AI.
+	 */
+	@Test
+	public void testOpenAiChat()
+	{	
+		//TODO
+	}
+	
+	/**
+	 * Test for POST method to get an dynamic request from Open AI.
+	 */
+	@Test
+	public void testAskOpenAi()
+	{	
+		//TODO
+	}
+	
+	/**
+	 * Test for Base get method that is used to call a caned request to Google Gen AI.
+	 */
+	@Test
+	public void testGoogleAiChat()
+	{	
+		//TODO
+	}
+	
+	/**
+	 * Test for POST method to get an dynamic request from Google Gen AI.
+	 */
+	@Test
+	public void testAskGoogleAi()
+	{	
+		//TODO
+	}
 }
+
